3da411173305151078633198a668c23aa2f2600f
Ricardo González <ricardoisrael.gonzalez@gmail.com>
2023-12-07 20:34:10 -0700

    Agrega CORS

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ const dotenv = require('dotenv').config()
 const { errorHandler} = require('./middlewares/errorMiddleware')
 const colors = require('colors')
 const connectDB = require('./config/db')
-
 const port = process.env.PORT || 3003
+const cors = require('cors')
+
 
 connectDB()
 
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
